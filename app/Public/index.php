044b7553991ed950dd31e2bf88912c13ea152e60
Cyprien <cyprien.forge@gmail.com>
2024-05-25 21:31:30 +0200

    Dockerise with PHP, MySQL, PHPMA & install composer

---
+++ app/Public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once('../build/vendor/autoload.php');
+use Router\Router;
+
+$router = new Router();
---
+++ build/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '23277b01a0bcc863884af734b4e83d167275816f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '23277b01a0bcc863884af734b4e83d167275816f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mistic100/randomcolor' => array(
+            'pretty_version' => '1.0.6',
+            'version' => '1.0.6.0',
+            'reference' => '527635e9d632ea216c91baf1ffb51f4c81d763de',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mistic100/randomcolor',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
